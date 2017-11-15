ed3c0bc622acd9b87c53a243d009f945ab23aac1
Muyinda Rogers <moverr@gmail.com>
2017-11-15 13:37:59 +0300

    Debugging

--- backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/account/AccountsEndpoint.java
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/account/AccountsEndpoint.java
@@ -7,18 +7,26 @@ package com.codemovers.scholar.v1.backoffice.api.v1.account;
 
 import com.codemovers.scholar.v1.backoffice.api.v1.abstracts.AbstractEndpoint;
 import com.codemovers.scholar.v1.backoffice.api.v1.account.entities.Account;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
 
 /**
  *
  * @author mover 11/15/2017
  */
+@Path("/")
 public class AccountsEndpoint extends AbstractEndpoint<Account> {
 
     public AccountsEndpoint(Class<Account> AbstractEndpoint) {
         super(AbstractEndpoint);
     }
 
-
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
     @Override
     public Class<Account> create(Account entity) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
