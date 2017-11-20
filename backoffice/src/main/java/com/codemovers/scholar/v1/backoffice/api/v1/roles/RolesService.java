387517697aff78d90e257f9a915c869a5fd2c021
Muyinda Rogers <moverr@gmail.com>
2017-11-20 15:20:00 +0300

    Role Jpa Controller

---
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/roles/RolesService.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.v1.backoffice.api.v1.roles;
+
+import com.codemovers.scholar.v1.backoffice.api.v1.abstracts.AbstractService;
+import com.codemovers.scholar.v1.backoffice.api.v1.roles.entities._Role;
+import com.codemovers.scholar.v1.backoffice.api.v1.users.UserService;
+import com.codemovers.scholar.v1.backoffice.db.controllers.RoleJpaController;
+import com.codemovers.scholar.v1.backoffice.db.controllers.UserJpaController;
+
+/**
+ *
+ * @author Manny
+ */
+public class RolesService extends AbstractService<_Role> {
+
+    private final RoleJpaController controller;
+
+    private static RolesService service = null;
+
+    public RolesService() {
+        controller = RoleJpaController.getInstance();
+    }
+
+    public static RolesService getInstance() {
+        if (service == null) {
+            service = new RolesService();
+        }
+        return service;
+    }
+
+
+    @Override
+    public _Role create(_Role entity) throws Exception {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public _Role getById(Integer Id) throws Exception {
+
+    }
+
+}
---
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/db/controllers/RoleJpaController.java
@@ -0,0 +1,149 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.v1.backoffice.db.controllers;
+
+import com.codemovers.scholar.v1.backoffice.db.JpaController;
+import static com.codemovers.scholar.v1.backoffice.db.controllers.GeneralAccountJpaController.LOG;
+import com.codemovers.scholar.v1.backoffice.db.entities.GeneralAccounts;
+import com.codemovers.scholar.v1.backoffice.db.entities.Roles;
+import com.codemovers.scholar.v1.backoffice.helper.Utilities;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.ws.rs.BadRequestException;
+
+/**
+ *
+ * @author Manny
+ */
+public class RoleJpaController extends JpaController {
+
+    private static RoleJpaController controller = null;
+
+    public static RoleJpaController getInstance() {
+        if (controller == null) {
+            controller = new RoleJpaController();
+        }
+        return controller;
+    }
+
+    public RoleJpaController() {
+        super(Roles.class);
+    }
+
+    public Roles create(Roles entity) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception eml) {
+            LOG.log(Level.INFO, eml.toString());
+            throw eml;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        return entity;
+
+    }
+
+    public void edit(Roles account) throws Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            account = em.merge(account);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = account.getId().intValue();
+                if (findRole(id) == null) {
+                    throw new BadRequestException("The mOffice with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public Roles findRole(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(Roles.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    private List<Roles> findRoleEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(GeneralAccounts.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<Roles> findRoleEntities() {
+        return findRoleEntities(true, -1, -1);
+    }
+
+    public List<Roles> findRoleEntities(int maxResults, int firstResult) {
+        return findRoleEntities(false, maxResults, firstResult);
+    }
+
+    public List<Roles> findByName(String name) {
+        List<Roles> RoleList = new ArrayList<>();
+        EntityManager em = getEntityManager();
+        Query query = em.createNamedQuery("Roles.findByName");
+        query.setParameter("name", name);
+        try {
+            RoleList = query.getResultList();
+            LOG.log(Level.FINE, "offices found for name {0}", new Object[]{name});
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "unexpected exception {0}\n{1}", new Object[]{ex.getMessage(), Utilities.getStackTrace(ex)});
+            return null;
+            // don't throw WebApplicationException, force caller to handle this
+        } finally {
+            LOG.log(Level.FINER, "closing entity manager {0}", em);
+            em.close();
+        }
+        return RoleList;
+    }
+
+
+    public int getCount() {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            Root<Roles> rt = cq.from(Roles.class);
+            cq.select(em.getCriteriaBuilder().count(rt));
+            Query q = em.createQuery(cq);
+            return (Integer) q.getSingleResult();
+        } finally {
+            em.close();
+        }
+    }
+
+}
