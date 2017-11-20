78d3d97b19e655337d3b4d1fa783fcb1610d8f47
Muyinda Rogers <moverr@gmail.com>
2017-11-21 02:08:23 +0300

    UserRole JPA Controller

---
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/db/controllers/UserRoleJpaController.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.v1.backoffice.db.controllers;
+
+import com.codemovers.scholar.v1.backoffice.db.JpaController;
+import com.codemovers.scholar.v1.backoffice.db.entities.UserRole;
+import com.codemovers.scholar.v1.backoffice.helper.Utilities;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.persistence.EntityManager;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.ws.rs.BadRequestException;
+
+/**
+ *
+ * @author Mover 11/21/2017
+ */
+public class UserRoleJpaController extends JpaController {
+    protected static final Logger LOG = Logger.getLogger(UserRoleJpaController.class.getName());
+
+    private static UserRoleJpaController controller = null;
+
+    public static UserRoleJpaController getInstance() {
+        if (controller == null) {
+            controller = new UserRoleJpaController();
+        }
+        return controller;
+    }
+
+    public UserRoleJpaController() {
+        super(UserRole.class);
+    }
+
+    public UserRole create(UserRole entity) {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            em.persist(entity);
+            em.getTransaction().commit();
+        } catch (Exception eml) {
+            LOG.log(Level.INFO, eml.toString());
+            throw eml;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+        return entity;
+
+    }
+
+    public void edit(UserRole userRole) throws Exception {
+        EntityManager em = null;
+        try {
+            em = getEntityManager();
+            em.getTransaction().begin();
+            userRole = em.merge(userRole);
+            em.getTransaction().commit();
+        } catch (Exception ex) {
+            String msg = ex.getLocalizedMessage();
+            if (msg == null || msg.length() == 0) {
+                Integer id = userRole.getId().intValue();
+                if (findById(id) == null) {
+                    throw new BadRequestException("The mOffice with id " + id + " no longer exists.");
+                }
+            }
+            throw ex;
+        } finally {
+            if (em != null) {
+                em.close();
+            }
+        }
+    }
+
+    public UserRole findById(Integer id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(UserRole.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    private List<UserRole> findUserRoleEntities(boolean all, int maxResults, int firstResult) {
+        EntityManager em = getEntityManager();
+        try {
+            CriteriaQuery cq = em.getCriteriaBuilder().createQuery();
+            cq.select(cq.from(UserRole.class));
+            Query q = em.createQuery(cq);
+            if (!all) {
+                q.setMaxResults(maxResults);
+                q.setFirstResult(firstResult);
+            }
+            return q.getResultList();
+        } finally {
+            em.close();
+        }
+    }
+
+    public List<UserRole> findUserRoleEntities() {
+        return findUserRoleEntities(true, -1, -1);
+    }
+
+    public List<UserRole> findUserRoleEntities(int maxResults, int firstResult) {
+        return findUserRoleEntities(false, maxResults, firstResult);
+    }
+
+    public List<UserRole> findByName(String name) {
+        List<UserRole> userRoletList = new ArrayList<>();
+        EntityManager em = getEntityManager();
+        Query query = em.createNamedQuery("MOffice.findByName");
+        query.setParameter("name", name);
+        try {
+            userRoletList = query.getResultList();
+            LOG.log(Level.FINE, "offices found for username {0}", new Object[]{name});
+        } catch (Exception ex) {
+            LOG.log(Level.SEVERE, "unexpected exception {0}\n{1}", new Object[]{ex.getMessage(), Utilities.getStackTrace(ex)});
+            return null;
+            // don't throw WebApplicationException, force caller to handle this
+        } finally {
+            LOG.log(Level.FINER, "closing entity manager {0}", em);
+            em.close();
+        }
+        return userRoletList;
+    }
+
+
+}
