91caadad3427b9942f76468b7cd68b3f053679e3
Muyinda Rogers <moverr@gmail.com>
2017-11-21 20:22:24 +0300

    Login Response

--- backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/accounts/entities/LoginResponse.java
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/accounts/entities/LoginResponse.java
@@ -1,44 +1,16 @@
 package com.codemovers.scholar.v1.backoffice.api.v1.accounts.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Objects;
 
 /**
  *
- * @author podolak
+ * @author Mover 11/21/2017
  */
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class LoginResponse {
 
     private String authentication;
-    private String token;
-    private String tenantId;
-
-    public String getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(String tenantId) {
-        this.tenantId = tenantId;
-    }
-
-    public LoginResponse(String authentication, String token, String tenantId) {
-        this.authentication = authentication;
-        this.token = token;
-        this.tenantId = tenantId;
-    }
-
-    public LoginResponse(String authentication, String token) {
-        this.authentication = authentication;
-        this.token = token;
-    }
-
-    public String getToken() {
-        return token;
-    }
-
-    public void setToken(String token) {
-        this.token = token;
-    }
 
     public LoginResponse() {
     }
@@ -53,6 +25,36 @@ public class LoginResponse {
 
     public void setAuthentication(String authentication) {
         this.authentication = authentication;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 97 * hash + Objects.hashCode(this.authentication);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final LoginResponse other = (LoginResponse) obj;
+        return Objects.equals(this.authentication, other.authentication);
+    }
+
+    @Override
+    public String toString() {
+        return this.getClass().getCanonicalName()
+                + "{"
+                + "authentication=" + authentication
+                + "}";
     }
 
 }
