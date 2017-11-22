cc41743620f67d6372406b6c598f9854589d2455
Muyinda Rogers <moverr@gmail.com>
2017-11-22 23:12:17 +0300

    Account Response

--- backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/accounts/AccountResponse.java
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/accounts/AccountResponse.java
@@ -5,10 +5,126 @@
  */
 package com.codemovers.scholar.v1.backoffice.api.v1.accounts;
 
+import java.util.Date;
+import java.util.Objects;
+
 /**
  *
  * @author Mover
  */
 class AccountResponse {
+
+    private String accounttype;
+    private String username;
+    private String emailaddress;
+    private String status;
+    private String scholarid;
+    private String authentication;
+
+    public AccountResponse() {
+    }
+
+    public String getAuthentication() {
+        return authentication;
+    }
+
+    public void setAuthentication(String authentication) {
+        this.authentication = authentication;
+    }
+
+    public String getAccounttype() {
+        return accounttype;
+    }
+
+    public void setAccounttype(String accounttype) {
+        this.accounttype = accounttype;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmailaddress() {
+        return emailaddress;
+    }
+
+    public void setEmailaddress(String emailaddress) {
+        this.emailaddress = emailaddress;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getScholarid() {
+        return scholarid;
+    }
+
+    public void setScholarid(String scholarid) {
+        this.scholarid = scholarid;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 3;
+        hash = 17 * hash + Objects.hashCode(this.accounttype);
+        hash = 17 * hash + Objects.hashCode(this.username);
+        hash = 17 * hash + Objects.hashCode(this.emailaddress);
+        hash = 17 * hash + Objects.hashCode(this.status);
+        hash = 17 * hash + Objects.hashCode(this.scholarid);
+        hash = 17 * hash + Objects.hashCode(this.authentication);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final AccountResponse other = (AccountResponse) obj;
+        if (!Objects.equals(this.accounttype, other.accounttype)) {
+            return false;
+        }
+        if (!Objects.equals(this.username, other.username)) {
+            return false;
+        }
+        if (!Objects.equals(this.emailaddress, other.emailaddress)) {
+            return false;
+        }
+        if (!Objects.equals(this.status, other.status)) {
+            return false;
+        }
+        if (!Objects.equals(this.scholarid, other.scholarid)) {
+            return false;
+        }
+        return Objects.equals(this.authentication, other.authentication);
+    }
+
+    @Override
+    public String toString() {
+        return "AccountResponse{"
+                + "accounttype=" + accounttype
+                + ", username=" + username
+                + ", emailaddress=" + emailaddress
+                + ", status=" + status
+                + ", scholarid=" + scholarid
+                + ", authentication=" + authentication
+                + "}";
+    }
+
 
 }
