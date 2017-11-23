e01aa0573263bb9a926190dc0ee9ba775344778f
Muyinda Rogers <moverr@gmail.com>
2017-11-23 19:29:07 +0300

    Permission Response

--- backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/accounts/GeneralAccountService.java
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/accounts/GeneralAccountService.java
@@ -7,6 +7,7 @@ package com.codemovers.scholar.v1.backoffice.api.v1.accounts;
 
 import com.codemovers.scholar.v1.backoffice.api.v1.abstracts.AbstractService;
 import com.codemovers.scholar.v1.backoffice.api.v1.accounts.entities.AuthenticationResponse;
+import com.codemovers.scholar.v1.backoffice.api.v1.accounts.entities.PermissionsResponse;
 import com.codemovers.scholar.v1.backoffice.api.v1.accounts.entities._Account;
 import com.codemovers.scholar.v1.backoffice.api.v1.accounts.entities._login;
 import com.codemovers.scholar.v1.backoffice.api.v1.contacts.ContactsService;
@@ -17,14 +18,21 @@ import com.codemovers.scholar.v1.backoffice.api.v1.users.entities.UserResponse;
 import com.codemovers.scholar.v1.backoffice.api.v1.users.entities._User;
 import com.codemovers.scholar.v1.backoffice.db.controllers.GeneralAccountJpaController;
 import com.codemovers.scholar.v1.backoffice.db.entities.GeneralAccounts;
+import com.codemovers.scholar.v1.backoffice.db.entities.Permissions;
 import com.codemovers.scholar.v1.backoffice.db.entities.Person;
+import com.codemovers.scholar.v1.backoffice.db.entities.Roles;
 import com.codemovers.scholar.v1.backoffice.db.entities.Users;
 import static com.codemovers.scholar.v1.backoffice.helper.Utilities.getNewExternalId;
 import com.codemovers.scholar.v1.backoffice.helper.enums.AccountType;
 import com.codemovers.scholar.v1.backoffice.helper.enums.ContactTypes;
 import com.codemovers.scholar.v1.backoffice.helper.enums.ParentTypes;
 import com.codemovers.scholar.v1.backoffice.helper.enums.StatusEnum;
+import java.security.Permission;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Date;
+import java.util.List;
+import java.util.Set;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.ws.rs.BadRequestException;
@@ -190,15 +198,41 @@ public class GeneralAccountService extends AbstractService<_Account, AccountResp
                 // todo : encrypt password
                 Users users = UserService.getInstance().login(login.getUsername(), login.getPassword(), logId);
 
-//                if (users == null) {
-//                    throw new BadRequestException("INVALID USERNAME AND OR PASSWORD ");
-//                } else {
-//                    // create response ::
-//                    authentication = UserService.getInstance().convertToBasicAuth(login.getUsername(), login.getPassword());
+                if (users == null) {
+                    throw new BadRequestException("INVALID USERNAME AND OR PASSWORD ");
+                } else {
+                    // create response ::
+                    authentication = UserService.getInstance().convertToBasicAuth(login.getUsername(), login.getPassword());
+
+                    response.setAuthentication(authentication);
+
+                    Set<Roles> roleslist = users.getRoles();
+
+                    List<PermissionsResponse> permissionsResponses = new ArrayList<>();
+
+//                      for (Roles r : roleslist) {
 //
-//                    response.setAuthentication(authentication);
-//                    // todo: generate response
-//                }
+//                        Set<Permissions> _permissionset = r.getPermissions();
+//
+//                        for (Permissions p : _permissionset) {
+//                            permissions.add(p);
+//                        }
+//
+//
+//                    }
+                    roleslist.stream().map((r) -> r.getPermissions()).forEachOrdered((_permissionset) -> {
+                        _permissionset.forEach((p) -> {
+                            PermissionsResponse permissionsResponse = new PermissionsResponse();
+                            permissionsResponse.setCode(p.getCode());
+                            permissionsResponse.setName(p.getName());
+
+                            permissionsResponses.add(permissionsResponse);
+                        });
+                    });
+
+                        response.setPermissions(permissionsResponses);
+
+                }
                 //todo : check username and password
             } else {
                 throw new BadRequestException(" USERNAME AND OR PASSWORD IS MANDATORY  ");
