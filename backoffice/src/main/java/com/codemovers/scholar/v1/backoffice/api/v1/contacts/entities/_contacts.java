7f8e2ad834733be0da8bd0dabd89357b469606ce
Muyinda Rogers <moverr@gmail.com>
2017-11-23 11:45:20 +0300

    Contacts Entity

---
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/contacts/entities/_contacts.java
@@ -0,0 +1,135 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.codemovers.scholar.v1.backoffice.api.v1.contacts.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.Objects;
+
+/**
+ *
+ * @author MOver 11/23/2017
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class _contacts {
+
+    private Integer id;
+    private String parentType;
+    private Integer parentId;
+    private String contactType;
+    private String details;
+    private String status;
+
+    public _contacts() {
+    }
+
+    public _contacts(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getParentType() {
+        return parentType;
+    }
+
+    public void setParentType(String parentType) {
+        this.parentType = parentType;
+    }
+
+    public Integer getParentId() {
+        return parentId;
+    }
+
+    public void setParentId(Integer parentId) {
+        this.parentId = parentId;
+    }
+
+    public String getContactType() {
+        return contactType;
+    }
+
+    public void setContactType(String contactType) {
+        this.contactType = contactType;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 11 * hash + Objects.hashCode(this.id);
+        hash = 11 * hash + Objects.hashCode(this.parentType);
+        hash = 11 * hash + Objects.hashCode(this.parentId);
+        hash = 11 * hash + Objects.hashCode(this.contactType);
+        hash = 11 * hash + Objects.hashCode(this.details);
+        hash = 11 * hash + Objects.hashCode(this.status);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final _contacts other = (_contacts) obj;
+        if (!Objects.equals(this.parentType, other.parentType)) {
+            return false;
+        }
+        if (!Objects.equals(this.contactType, other.contactType)) {
+            return false;
+        }
+        if (!Objects.equals(this.details, other.details)) {
+            return false;
+        }
+        if (!Objects.equals(this.status, other.status)) {
+            return false;
+        }
+        if (!Objects.equals(this.id, other.id)) {
+            return false;
+        }
+        return Objects.equals(this.parentId, other.parentId);
+    }
+
+    @Override
+    public String toString() {
+        return "contacts{"
+                + "id=" + id
+                + ", parentType=" + parentType
+                + ", parentId=" + parentId
+                + ", contactType=" + contactType
+                + ", details=" + details
+                + ", status=" + status
+                + "}";
+    }
+
+
+}
