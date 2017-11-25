ca68886239c16e8b8697ce4e2116ff8693e487d9
Muyinda Rogers <moverr@gmail.com>
2017-11-26 02:06:29 +0300

    School Server connection Endpoint and Service

--- backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/serverconnections/SchoolServerConnectionEndpoint.java
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/serverconnections/SchoolServerConnectionEndpoint.java
@@ -5,10 +5,52 @@
  */
 package com.codemovers.scholar.v1.backoffice.api.v1.serverconnections;
 
+import com.codemovers.scholar.v1.backoffice.api.v1.abstracts.AbstractEndpoint;
+import com.codemovers.scholar.v1.backoffice.api.v1.abstracts.AbstractService;
+import com.codemovers.scholar.v1.backoffice.api.v1.schoolaccounts.SchoolAccountService;
+import com.codemovers.scholar.v1.backoffice.api.v1.schoolaccounts.SchoolAccountsEndpoint;
+import com.codemovers.scholar.v1.backoffice.api.v1.serverconnections.entities.SchoolServerConnectionResponse;
+import com.codemovers.scholar.v1.backoffice.api.v1.serverconnections.entities._SchoolServerConnection;
+import java.util.Collection;
+import java.util.logging.Logger;
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
 /**
  *
  * @author Mover 11/26/2017
  */
-public class SchoolServerConnectionEndpoint {
+public class SchoolServerConnectionEndpoint extends AbstractEndpoint<_SchoolServerConnection, SchoolServerConnectionResponse> {
+
+    private static final Logger LOG = Logger.getLogger(SchoolAccountsEndpoint.class.getName());
+    @Context
+    private ContainerRequestContext context;
+
+    private SchoolServerConnectionService service = null;
+
+    public SchoolServerConnectionEndpoint() {
+        service = SchoolServerConnectionService.getInstance();
+    }
+
+    @Override
+    public SchoolServerConnectionResponse update(Integer id, _SchoolServerConnection entity) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Response archive(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Response delete(Integer id) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Collection<SchoolServerConnectionResponse> list(int start, int end) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 
 }
--- backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/serverconnections/SchoolServerConnectionService.java
+++ backoffice/src/main/java/com/codemovers/scholar/v1/backoffice/api/v1/serverconnections/SchoolServerConnectionService.java
@@ -5,10 +5,37 @@
  */
 package com.codemovers.scholar.v1.backoffice.api.v1.serverconnections;
 
+import com.codemovers.scholar.v1.backoffice.api.v1.abstracts.AbstractService;
+import com.codemovers.scholar.v1.backoffice.api.v1.schoolaccounts.SchoolAccountService;
+import com.codemovers.scholar.v1.backoffice.api.v1.serverconnections.entities.SchoolServerConnectionResponse;
+import com.codemovers.scholar.v1.backoffice.api.v1.serverconnections.entities._SchoolServerConnection;
+import com.codemovers.scholar.v1.backoffice.db.controllers.SchoolAccountJpaController;
+import com.codemovers.scholar.v1.backoffice.db.controllers.SchoolServerConnectionJpaController;
+import com.codemovers.scholar.v1.backoffice.db.entities.GeneralAccounts;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Mover 11/26/2017
  */
-public class SchoolServerConnectionService {
+public class SchoolServerConnectionService extends AbstractService<_SchoolServerConnection, SchoolServerConnectionResponse> {
+
+    private static final Logger LOG = Logger.getLogger(SchoolServerConnectionService.class.getName());
+
+    private static SchoolServerConnectionService service = null;
+
+    GeneralAccounts general_account = null;
+    private final SchoolServerConnectionJpaController controller;
+
+    public SchoolServerConnectionService() {
+        controller = new SchoolServerConnectionJpaController();
+    }
+
+    public static SchoolServerConnectionService getInstance() {
+        if (service == null) {
+            service = new SchoolServerConnectionService();
+        }
+        return service;
+    }
 
 }
